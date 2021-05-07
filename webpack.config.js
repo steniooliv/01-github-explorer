1cf65ee0f18c8c2ae238ce9fed464d03be7ee8ba
Stenio Oliveira <steniooliv@gmail.com>
2021-05-06 23:23:03 -0300

    create react structure and fast reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
   module: {
     rules: [
       {
