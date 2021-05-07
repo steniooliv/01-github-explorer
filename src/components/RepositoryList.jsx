807b95386dba9f76b0ade4db94f46fce35ea4e50
Stenio Oliveira <steniooliv@gmail.com>
2021-05-07 01:03:12 -0300

    create component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,22 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'Unform',
+  description: 'Forms in React',
+  link: '#'
+}
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+      </ul>
+    </section>
+  );
+}
