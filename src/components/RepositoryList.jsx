d7113fdc0f57059ad3e9d3b50e89b536d7a6283b
Stenio Oliveira <steniooliv@gmail.com>
2021-05-17 22:28:08 -0300

    add api to populate the repository list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,25 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-  name: 'Unform',
-  description: 'Forms in React',
-  link: '#'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/steniooliv/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
